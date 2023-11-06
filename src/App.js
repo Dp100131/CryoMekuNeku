d83abf662269a3c06bd83a979bf8646306b43aba
Daniel Pinto <daniel2190558@correo.uis.edu.co>
2023-11-06 16:36:55 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import { HomePage } from './Pages/HomePage';
 import { DetailPage } from './Pages/DetailPage';
 import { LogInPage } from './Pages/LogInPage';
 import { ProfilePage } from './Pages/ProfilePage';
-import {HashRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return ( 
-    <HashRouter>
-      <div className="min-h-screen flex flex-col">
-        <Header></Header>
-        <div className='flex-1'>
+    <BrowserRouter> 
+        <div className="min-h-screen flex flex-col">
+          <Header></Header>
+          <div className="flex-1">
             <Routes>
               <Route path='/' element={ <HomePage/>}/>
               <Route path='/detail' element={ <DetailPage/>}/>
               <Route path='/LogIn' element={ <LogInPage/>}/>
               <Route path='/profile' element={ <ProfilePage/>}/>
-            </Routes> 
-        </div>
-        <Footer></Footer>
-      </div> 
-    </HashRouter> 
+            </Routes>
+          </div>
+          <Footer></Footer> 
+        </div>  
+    </BrowserRouter> 
   )
 }
 
